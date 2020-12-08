631db779128c35d0c296295d0c2e0a6a2656ae4d
Perry Sittser <sittserp@gmail.com>
2020-12-08 15:44:44 -0800

    class methods finished

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
+
 const Movie = require('./lib/utils/models/Movie.js');
-// require('dotenv').config();
 
 // const express = require('express');
 // const app = express();
@@ -7,7 +8,8 @@ const Movie = require('./lib/utils/models/Movie.js');
 // app.use(express.json());
 
 Movie
-    .insert({ title: 'Star Wars', description: 'A New Hope', url: 'http://url.com' })
-    .then(console.log);
+  .find() 
+  .then(console.log);
 
 // app.listen('5432', () => { console.log('listening on port 5432') })
+
